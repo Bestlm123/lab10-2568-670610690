80e34fa0dcb7a8e26160396bc3fd344c80202006
Bestlm123 <best.nattapat18@gmail.com>
2025-08-09 13:49:47 +0700

    lab 10 completed

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,21 +1,29 @@
 import { useState } from "react";
 import { UserCardDetail } from "./UserCardDetail";
 import { BsChevronUp, BsChevronDown } from "react-icons/bs";
-export const UserCard = ({ name, imgUrl, address, email }) => {
+
+interface UserCardProps {
+  name: string;
+  imgUrl: string;
+  address: string;
+  email: string;
+}
+
+export const UserCard = ({ name, imgUrl, address, email }: UserCardProps) => {
   const [isDetailShown, setIsDetailShown] = useState(false);
 
   const userCardOnClick = () => {
     setIsDetailShown(!isDetailShown);
   };
-
   return (
     <div className="border-bottom">
       <div className="d-flex align-items-center p-3" onClick={userCardOnClick}>
-        <img src={imgUrl} width="90px" className="rounded-circle me-4"></img>
+        <img src={imgUrl} width="90px" className="rounded-circle me-4" />
         <span className="text-center display-6 me-auto">{name}</span>
         {isDetailShown ? <BsChevronUp /> : <BsChevronDown />}
       </div>
       {/* display UserCardDetail accordingly */}
+      {isDetailShown && <UserCardDetail address={address} email={email} />}
     </div>
   );
 };
